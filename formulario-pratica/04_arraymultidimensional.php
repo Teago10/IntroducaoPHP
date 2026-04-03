335f44e6577d35c975c50b51022c8d09bb9e4895
thiago Affonso <thiagoaffonso452@gmail.com>
2026-04-02 21:56:41 -0300

    deixando o codigo mais limpo e testo outro

--- formulario-pratica/04_arraymultidimensional.php
+++ formulario-pratica/04_arraymultidimensional.php
@@ -4,9 +4,10 @@
 
     if(isset($_POST["uf"], $_POST["estado"])){
         $estados[] = [strtoupper($_POST["uf"]), $_POST["estado"]];
+        $estados[] = [strtoupper($_POST["uf2"]), $_POST["estado2"]];
 
         for($i = 0; $i < count($estados);$i++){
-            echo "UF: {$estados[$i][0]},<br>  Estado: {$estados[$i][1]}";
+            echo "UF: {$estados[$i][0]},<br>  Estado: {$estados[$i][1]}<br><br>";
         }
 
         echo '<br><a href="04_arraymultidimensional.html">Voltar</a>';
